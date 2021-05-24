f6e5948235a6833dd0f5dbad4d6dad294c4e6b68
Wentao Wu <wentao.wu@mobilewareinc.com>
2021-05-23 21:45:59 -0400

    Initial Commit

---
+++ InstagramSwiftUIMock/InstagramSwiftUIMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIMockApp.swift
+//  InstagramSwiftUIMock
+//
+//  Created by Wentao Wu on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
